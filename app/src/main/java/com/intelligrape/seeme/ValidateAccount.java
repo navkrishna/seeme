1de95448f798894e6797cf4bbe9de5d0ba827cc1
navkrishna <navkrishna@intelligrape.com>
2015-02-05 23:29:59 +0530

    account validation api implementation completed

--- app/src/main/java/com/intelligrape/seeme/ValidateAccount.java
+++ app/src/main/java/com/intelligrape/seeme/ValidateAccount.java
@@ -1,13 +1,74 @@
 package com.intelligrape.seeme;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import com.intelligrape.seeme.loader.APICaller;
+import com.intelligrape.seeme.loader.LoaderCallback;
+import com.intelligrape.seeme.model.Model;
+import com.intelligrape.seeme.model.Request;
+import com.intelligrape.seeme.parser.MessageParser;
+import com.intelligrape.seeme.utility.ApiDetails;
+import com.intelligrape.seeme.utility.AppConstants;
+import com.intelligrape.seeme.utility.PrefStore;
+import com.intelligrape.seeme.utility.Utility;
+
+import java.util.HashMap;
 
 
 public class ValidateAccount extends BaseActivity {
+    private EditText mEtCode;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_validate_account);
+        setToolbar(R.id.toolbar);
+        findViewsAndAddListener();
+    }
+
+    private void findViewsAndAddListener() {
+        mEtCode = (EditText) findViewById(R.id.et_code);
+
+        findViewById(R.id.btn_verify_account).setOnClickListener(mOnClickListener);
+    }
+
+    private View.OnClickListener mOnClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            switch (v.getId()) {
+                case R.id.btn_verify_account:
+                    requestVerifyAccount();
+                    break;
+            }
+        }
+    };
+
+    private void requestVerifyAccount() {
+        HashMap<String, String> paramMap = new HashMap<>();
+        paramMap.put("verificationToken", Utility.getText(mEtCode));
+        paramMap.put("email", PrefStore.getString(mActivity, AppConstants.PREF_KEY_EMAIL_ID));
+        paramMap.put("actionName", ApiDetails.ACTION_VERIFY_ACCOUNT);
+        Request request = new Request();
+        request.setDialogMessage(getString(R.string.progress_dialog_msg));
+        request.setParamMap(paramMap);
+        request.setUrl(ApiDetails.HOME_URL);
+        request.setRequestType(Request.HttpRequestType.POST);
+        LoaderCallback loaderCallback = new LoaderCallback(mActivity, new MessageParser());
+        loaderCallback.requestToServer(request);
+        loaderCallback.setServerResponse(new APICaller() {
+            @Override
+            public void onComplete(Model model) {
+                if (model.getStatus() == 1) {
+                    PrefStore.setBoolean(mActivity, AppConstants.PREF_KEY_IS_ACCOUNT_VERIFIED, true);
+                    Utility.showToastMessage(mActivity, model.getMessage());
+//                    startActivity(new Intent(mActivity, ValidateAccount.class));
+                } else {
+                    Utility.showToastMessage(mActivity, model.getMessage());
+                }
+            }
+        });
+
     }
 }
